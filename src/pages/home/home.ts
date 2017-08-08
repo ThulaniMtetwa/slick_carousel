a5c0ca9b1553a7a928127a2432ca9f270dfd1314
Thulani Mtetwa <thulanimtetwa@Thulanis-MacBook-Pro.local>
2017-08-08 10:18:23 +0200

    Waterfall carousel ionic 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as $ from "jquery";
+import 'slick-carousel/slick/slick';
+
 
 @Component({
   selector: 'page-home',
@@ -9,6 +12,42 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    $('.myCarousel').slick({
+      // dots: true,
+      // centerMode: true,
+      // infinite: false,
+      // centerPadding: '60px',
+      // slidesToShow: 1
+
+      centerMode: true,
+      centerPadding: '20px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: true,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: true,
+            centerMode: true,
+            centerPadding: '70px',
+            slidesToShow: 1,
+            focusOnSelect: true,
+            infinite: true
+          }
+        }
+      ]
+    });
   }
 
 }
